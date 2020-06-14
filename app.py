bb016be7f72b2862fc7f5aefc46962377ea772fb
s201731050-kfupm <s201731050@kfupm.edu.sa>
2020-06-14 20:35:33 +0300

    feat: Implementing models and test endpoints
    - models.py is implemented.
    - db init is working.
    - db migrate has an error.
    - app.py has a the homepage as an endpoint test.

--- app.py
+++ app.py
@@ -2,16 +2,36 @@ import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from models import Movie , Actor ,setup_db
+from flask_moment import Moment
 
 def create_app(test_config=None):
   # create and configure the app --heroku config --app
   # testing again
   app = Flask(__name__)
   CORS(app)
+  moment = Moment(app)
+  app.config.from_object('config')
+  setup_db(app)
+  cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+  @app.after_request
+  def after_request(response):
+    response.headers.add('Access-Contro-Allow-Headers','Content-Type ,Authorization')
+    response.headers.add('Access-Contro-Allow-Headers','GET, POST ,PATCH , DELETE ,OPTIONS')
+    response.headers.add('Access-Control-Allow-Origin' ,  'http://localhost:5000')
+    return response 
+  
+  @app.route('/', methods=['GET'])
+  def test_api():
+    return jsonify({
+      'success': True ,
+      'message' : "hello world "
+    })
 
   return app
 
-APP = create_app()
+app = create_app()
 
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
